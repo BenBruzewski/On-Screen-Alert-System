06846b9881e45019fc63fbd6b3bde3cfa8c22454
bs0094 <bs0094@uah.edu>
2023-03-07 13:09:38 -0600

    Made second "running" pop-up box, added some buttons, and completely works with modifying a config file

--- basic gui tkinter.py
+++ basic gui tkinter.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from configparser import ConfigParser
 
+# create the main window and its dimensions
 window = tk.Tk()
 window.title("On Screen Alert System")
 window.configure(background="white") # this background can accept hexRGB values (eg. #6FAFE7)
@@ -8,6 +10,23 @@ window.maxsize(900, 600)
 w, h = window.winfo_screenwidth(), window.winfo_screenheight()
 window.geometry("900x600+%d+%d" % ((w/2)-450, (h/2)-300)) # starting dims and x/y coords
 pNum = "-"
+# create the pop-up window and its dimensions
+global runningWindow
+runningWindow = tk.Toplevel()
+runningWindow.minsize(400,400)
+runningWindow.maxsize(400,400)
+runningWindow.title("OSAS is running...")
+runningWindow.configure(background="grey")
+runningWindow.geometry("400x400+%d+%d" % ((w/2)-200, (h/2)-300))
+# create the configparsing object
+confObj = ConfigParser()
+
+# example of reading values and modifying them
+# confObj.read("config.ini")
+# trackedImg = confObj["TRACKEDIMAGES"]
+# trackedImg["discord_call_half"] = "0"
+# with open('config.ini', 'w') as conf:
+#     confObj.write(conf)
 
 # handler to accept user's update from phone number text box
 def handle_pNum_update_press():
@@ -16,8 +35,33 @@ def handle_pNum_update_press():
 
 # currently debugs output into terminal
 def handle_button_press():
-    print("button pressed. pNum val =", pNum)
-    print("checkboxes values are noted as: %d, %d, %d, %d" % (discCallBox.get(), discCallHalfBox.get(), discTextBox.get(), fireWBox.get()))
+    runPhoneLabel.configure(text=outVal.cget("text"))
+    runCB2Label.configure(text="%d, %d, %d, %d, %d" % (discCallBox.get(), discCallHalfBox.get(), discTextBox.get(), fireWBox.get(), custWBox.get()))
+    # modify the config file
+    confObj.read("config.ini")
+    trackedImg = confObj["TRACKEDIMAGES"]
+    trackedImg["discord_call_full"] = "%d" % discCallBox.get()
+    trackedImg["discord_call_half"] = "%d" % discCallHalfBox.get()
+    trackedImg["discord_text"] = "%d" % discTextBox.get()
+    trackedImg["windows_firewall"] = "%d" % fireWBox.get()
+    trackedImg["custom_notif"] = "%d" % custWBox.get()
+    with open('config.ini', 'w') as conf:
+        confObj.write(conf)
+    window.withdraw()
+    runningWindow.deiconify()
+
+# function that runs whether the user presses the "X" in the top right or if they hit the "Cancel" button
+def on_closing():
+    print("\nreturning to main window...")
+    runningWindow.withdraw()
+    window.deiconify()
+
+def on_quit():
+    print("Goodbye.")
+    window.destroy()
+
+# this single line overwrites the control that would normally happen when the user hits the pop-up screen's "X" button
+runningWindow.protocol("WM_DELETE_WINDOW", on_closing)
 
 # image storage
 osasIMG = tk.PhotoImage(file="01.gif")
@@ -35,8 +79,9 @@ rightFrame = tk.Frame(CF, bg='grey')
 outNotifText = tk.Label(rightFrame, text="Program Status:").pack(fill=tk.BOTH)
 inNotifText = tk.Label(leftFrame, text="User Input(s):").pack(fill=tk.BOTH)
 
-
+# ----------------------------------------------------------------
 # ----------------------- LEFT SIDE OF GUI -----------------------
+# ----------------------------------------------------------------
 # phone entry frame
 phoneFrame = tk.Frame(leftFrame)
 phoneButton = tk.Button(phoneFrame, text="Submit", command=handle_pNum_update_press).pack(side='right')
@@ -50,24 +95,34 @@ discCallBox = tk.IntVar()
 discCallHalfBox = tk.IntVar()
 discTextBox = tk.IntVar()
 fireWBox = tk.IntVar()
+custWBox = tk.IntVar()
 dcChk = tk.Checkbutton(checkFrame, text="Discord Call", var=discCallBox, bg='lightgrey')
 dchChk = tk.Checkbutton(checkFrame, text="Discord Call (Half Size)", var=discCallHalfBox, bg='lightgrey')
 dtChk = tk.Checkbutton(checkFrame, text="Discord Text", var=discTextBox, bg='lightgrey')
 fwChk = tk.Checkbutton(checkFrame, text="Firewall Notification", var=fireWBox, bg='lightgrey')
+custChk = tk.Checkbutton(checkFrame, text="Custom Notification(s)", var=custWBox, bg='lightgrey')
 dcChk.pack(anchor='w')
 dchChk.pack(anchor = 'w')
 dtChk.pack(anchor = 'w')
 fwChk.pack(anchor = 'w')
+custChk.pack(anchor = 'w')
 checkFrame.pack(expand=tk.TRUE)
 
-# "Start" button
-startFrame = tk.Frame(leftFrame)
-startButton = tk.Button(startFrame, text="Start", command=handle_button_press).pack()
+# "Start" and "Close" button
+startFrame = tk.Frame(leftFrame, bg='lightgrey')
+startButton = tk.Button(startFrame, text="Start", command=handle_button_press)
+startButton.pack()
+bufFrame3 = tk.Frame(startFrame, height=10, bg='lightgrey').pack()
+closeButton = tk.Button(startFrame, text="Close", command=on_quit)
+closeButton.pack()
 startFrame.pack(expand=tk.TRUE)
+# ----------------------------------------------------------------
 # ----------------------------------------------------------------
 
 
-# ----------------------- RIGHT SIDE OF GUI -----------------------
+# ----------------------------------------------------------------
+# ----------------------- RIGHT SIDE OF GUI ----------------------
+# ----------------------------------------------------------------
 # status basic output (repeat what is put in phone entry blank)
 statusFrame = tk.Frame(rightFrame, bg='grey')
 outLabel = tk.Label(statusFrame, text="Value submitted:", bg='grey')
@@ -75,6 +130,7 @@ outLabel.pack()
 outVal = tk.Label(statusFrame, text=pNum, bg='grey')
 outVal.pack()
 statusFrame.pack(expand=tk.TRUE)
+# ----------------------------------------------------------------
 # -----------------------------------------------------------------
 
 
@@ -83,5 +139,44 @@ leftFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
 rightFrame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=tk.TRUE)
 CF.pack(fill=tk.BOTH, expand=tk.TRUE)
 
-# launch the actual window
+# ----------------------------------------------------------------
+# ----------------------- POP-UP WINDOW :) -----------------------
+# ----------------------------------------------------------------
+# this window will eventually show everything that OSAS will be doing during runtime
+# for now, it will just have the "phone number" entered, the checkbox values, and the cancel button
+runningFrame = tk.Frame(runningWindow, bg='grey')
+
+# status window listing phone number
+runPhoneFrame = tk.Frame(runningFrame, bg='grey')
+runLabel = tk.Label(runPhoneFrame, text="Phone Number Chosen:", bg='grey')
+runLabel.pack()
+runPhoneLabel = tk.Label(runPhoneFrame, text="youwontseethisbuffer", bg='grey')
+runPhoneLabel.pack()
+runPhoneFrame.pack()
+
+# buffer frame for spacing
+bufFrame = tk.Frame(runningFrame, height=20, bg='grey').pack()
+
+# status window listing checkbox choices (currently just integer values)
+runCheckboxFrame = tk.Frame(runningFrame, bg='grey')
+runCBLabel = tk.Label(runCheckboxFrame, text="Array of detected options selected:", bg='grey')
+runCBLabel.pack()
+runCB2Label = tk.Label(runCheckboxFrame, text="youwontseethisbuffer", bg='grey')
+runCB2Label.pack()
+runCheckboxFrame.pack()
+
+# buffer frame for spacing
+bufFrame2 = tk.Frame(runningFrame, height=100, bg='grey').pack()
+
+# cancel button
+runButtonFrame = tk.Frame(runningFrame, bg='grey')
+cancelButton = tk.Button(runButtonFrame, text="Cancel", command=on_closing)
+cancelButton.pack()
+runButtonFrame.pack(expand=tk.TRUE)
+
+runningFrame.pack(expand=tk.TRUE)
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+# start Tkinter
 window.mainloop()
